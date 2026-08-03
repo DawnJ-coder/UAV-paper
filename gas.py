4e214e309bd33ac0d60c97e722dc7283171e516b
DawnJ-coder <2139571082@qq.com>
2026-08-03 15:20:33 +0800

    Update gas.py

--- gas.py
+++ gas.py
@@ -1,3 +1,5 @@
 center_root_dir = r"D:\gas\beamform_results_gc_cs"
 offset_near_root_dir = r"D:\gas\beamform_results_offset_multiple_gc_cs_0001"
 offset_far_root_dir = r"D:\gas\beamform_results_offset_multiple_gc_gy_cs_0001"
+
+"D:\gas\beamform_results_offset_multiple_gc_0001\0.2mm铜管泄漏_100kPa_690sccm_2.0m_null\0.2mm铜管泄漏_100kPa_690sccm_2.0m_null_00_00d5_down_beamform_result.wav"
